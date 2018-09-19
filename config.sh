f0dcb90872e8fdd0609d72144add94a4a7dde332
unblockcnrelease <40509577+unblockcnrelease@users.noreply.github.com>
2018-09-19 17:20:31 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,3 +2,8 @@ curl -sL https://rpm.nodesource.com/setup_8.x | bash -
 yum install -y nodejs
 npm config set registry https://registry.npm.taobao.org
 yum install -y git
+wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
+yum localinstall -y mysql57-community-release-el7-8.noarch.rpm
+yum install -y mysql-community-server
+systemctl start mysqld
+ssh-keygen -t rsa
