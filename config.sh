8f321211b7c67e1b3be7524f6fdf28d9aa7c46da
unblockcnrelease <40509577+unblockcnrelease@users.noreply.github.com>
2018-09-19 16:03:59 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,4 @@
 curl -sL https://rpm.nodesource.com/setup_8.x | bash -
 yum install -y nodejs
 npm config set registry https://registry.npm.taobao.org
-npm install -g nodemon
-npm install -g typescript
-npm install -g ts-node
 yum install -y git
